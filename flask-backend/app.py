9ec378b633fe86b3d2d8a79174d5ac96b24962a6
babs3 <barbaraema3@gmail.com>
2025-03-17 12:52:45 +0000

    good separation of backend and frontend

---
+++ flask-backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from models import db  # Assuming models.py is in the same folder
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@db/{os.getenv("POSTGRES_DB")}'
+app.config["JWT_SECRET_KEY"] = "super-secret-key"  # Change in production!
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)  # For database migrations
+jwt = JWTManager(app)
